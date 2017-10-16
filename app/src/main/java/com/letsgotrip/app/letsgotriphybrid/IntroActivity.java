563412ac38ce948d89f2e538534085c51c2c04fa
Administrator <nightbug@naver.com>
2017-10-16 13:36:31 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/letsgotrip/app/letsgotriphybrid/MainActivity.java

--- app/src/main/java/com/letsgotrip/app/letsgotriphybrid/IntroActivity.java
+++ app/src/main/java/com/letsgotrip/app/letsgotriphybrid/IntroActivity.java
@@ -1,11 +1,17 @@
 package com.letsgotrip.app.letsgotriphybrid;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.view.Window;
 
@@ -17,6 +23,13 @@ public class IntroActivity extends Activity {
     Handler handler;//핸들러 선언
     private View decorView;
     private int	uiOption;
+
+    // 사용자 위치 수신기
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    double latitude;
+    double longitude;
 
     Runnable runnable = new Runnable() {
         @Override
@@ -44,10 +57,10 @@ public class IntroActivity extends Activity {
             uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
         decorView.setSystemUiVisibility( uiOption );
-
         init();
+        getMyLocation();
 
-        handler.postDelayed(runnable, 1000);
+//        handler.postDelayed(runnable, 1000);
     }
 
     public void init() {
@@ -58,5 +71,71 @@ public class IntroActivity extends Activity {
     public void onBackPressed(){
         super.onBackPressed();
         handler.removeCallbacks(runnable);
+    }
+
+
+
+
+
+    /**
+     * 사용자의 위치를 수신
+     */
+    public Location getMyLocation() {
+        Location currentLocation = null;
+        // Register the listener with the Location Manager to receive location updates
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 사용자 권한 요청
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2000);
+        }
+        else {
+            handler.postDelayed(runnable, 100);
+        }
+
+        return currentLocation;
+    }
+
+    /**
+     * GPS 를 받기 위한 매니저와 리스너 설정
+     */
+    public void settingGPS() {
+        // Acquire a reference to the system Location Manager
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                // TODO 위도, 경도로 하고 싶은 것
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+    }
+
+    /**
+     * GPS 권한 응답에 따른 처리
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    boolean canReadLocation = false;
+    public void onRequestPermissionsResult(int requestCode,String[] permissions,int[] grantResults) {
+        if (requestCode == 2000) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+// success!
+                handler.postDelayed(runnable, 100);
+                canReadLocation = true;
+            } else {
+// Permission was denied or request was cancelled
+                canReadLocation = false;
+            }
+        }
     }
 }
